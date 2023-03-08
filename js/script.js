61cda73bad61e8401d8a55844d62254d55b28fa3
Katarzyna Wyrąbkiewicz <katarzyna.wyrabkiewicz@gmail.com>
2023-03-08 13:37:28 +0100

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+function titleClickHandler(event) {
+  event.preventDefault();
+  const clickedElement = this;
+
+  /* [DONE] remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .post.active');
+
+  for(let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
+  /* [DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+}
